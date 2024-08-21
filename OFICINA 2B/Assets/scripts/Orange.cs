05bad9a252d6fc3c9b57104edaa22e260a670a81
EvilinBeatriz <145377248+EvilinBeatriz@users.noreply.github.com>
2024-08-21 17:46:13 -0300

    mais atualizações

--- OFICINA 2B/Assets/scripts/Orange.cs
+++ OFICINA 2B/Assets/scripts/Orange.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Orange : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+    public GameObject collected;
+    public int Score;
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
         
     }
 
@@ -20,7 +26,13 @@ public class Orange : MonoBehaviour
     {
         if(collider.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+            GameController.instance.totalScore += Score;
+             GameController.instance.UpdateScoreText();
+
+            Destroy(gameObject, 0.25f);
         }
     }
 }
